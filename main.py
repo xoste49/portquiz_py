7636118487ae2a224d1feb452e14acf024387f0e
Pavel <xoste49@gmail.com>
2021-05-13 17:42:59 +0300

    Add comments

--- main.py
+++ main.py
@@ -1,33 +1,43 @@
-import socket
-from datetime import time
-
 import requests
 
 open_ports = []
 closed_ports = []
 
-def scan_ports(min, max):
-    for port in range(min, max + 1):
+
+def scan_ports(from_port, to_port):
+    """
+    Диапазон сканирования
+    :param from_port: Начальный порт сканирования
+    :param to_port: Конечный порт сканирования
+    """
+    for port in range(from_port, to_port + 1):
         portscan(port)
 
 
 def portscan(port):
+    """
+    Сканирование указанного порта
+    :param port: порт сканирования
+    :return: результат сканирования порта
+    """
     try:
-        r = requests.get(f"http://portquiz.net:{port}", timeout=10)
+        r = requests.get(f"http://portquiz.net:{port}", timeout=1)
         if r.status_code == 200:
-            # print('Port :', port, "is open.")
             open_ports.append(f"{port}, ")
-            print('Port :', port, " открыт ", r.status_code)
+            print('Port:', port, "is open.")
         else:
             closed_ports.append(f"{port}, ")
+            print('Port:', port, "is closed.")
         r.close()
-    except:
-        pass
-
+    except requests.ConnectTimeout as e:
+        closed_ports.append(f"{port}, ")
+        print('Port:', port, "is closed.")
+    except Exception as e:
+        print(e.message)
 
 
 if __name__ == '__main__':
-    scan_ports(1, 65355)
+    scan_ports(1000, 65355)
     print("Открытые порты")
     print(', '.join(open_ports))
     print("Закрытые порты")
